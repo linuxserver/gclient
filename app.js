5e5d9e5f343f7f91e467564579c3f7953ee98d8f
thelamer <ryankuba@gmail.com>
2022-01-28 15:49:31 -0800

    add basic functionality file manager with pam auth

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // LinuxServer Guacamole Client
 
 //// Application Variables ////
-var baseurl = process.env.SUBFOLDER || '/';
+var baseUrl = process.env.SUBFOLDER || '/';
 var socketIO = require('socket.io');
 var crypto = require('crypto');
 var ejs = require('ejs');
@@ -9,12 +9,14 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
-var { pamAuthenticate, pamErrors } = require('node-linux-pam');
+var { pamAuthenticatePromise } = require('node-linux-pam');
 var CUSTOM_PORT = process.env.CUSTOM_PORT || 3000;
+var USER = process.env.CUSTOM_USER || 'abc';
 var PASSWORD = process.env.PASSWORD || 'abc';
 var FORCE_LOGIN = process.env.FORCE_LOGIN || false;
 var baseRouter = express.Router();
-var fs = require('fs').promises;
+var fsw = require('fs').promises;
+var fs = require('fs');
 
 ///// Guac Websocket Tunnel ////
 var GuacamoleLite = require('guacamole-lite');
@@ -27,8 +29,10 @@ var clientOptions = {
     verbose: false
   }
 };
+
 // Spinup the Guac websocket proxy on port 3000 if guacd is running
-var guacServer = new GuacamoleLite({server: http,path:baseurl +'guaclite'},{host:'127.0.0.1',port:4822},clientOptions);
+var guacServer = new GuacamoleLite({server: http,path:baseUrl +'guaclite'},{host:'127.0.0.1',port:4822},clientOptions);
+
 // Function needed to encrypt the token string for guacamole connections
 var encrypt = (value) => {
   var iv = crypto.randomBytes(16);
@@ -44,6 +48,7 @@ var encrypt = (value) => {
 
 //// Public JS and CSS ////
 baseRouter.use(express.static(__dirname + '/public'));
+
 //// Embedded guac ////
 baseRouter.get('/', function (req, res) {
   let connectString = {
@@ -58,32 +63,135 @@ baseRouter.get('/', function (req, res) {
     }
   };
   if ((! req.query.login) && (FORCE_LOGIN == false)) {
-    Object.assign(connectString.connection.settings, {'username':'abc','password':PASSWORD});
+    Object.assign(connectString.connection.settings, {'username':USER,'password':PASSWORD});
   }
   res.render(__dirname + '/rdp.ejs', {token : encrypt(connectString)});
 });
+
 //// Web File Browser ////
+// Send landing page 
 baseRouter.get('/files', function (req, res) {
   res.sendFile( __dirname + '/public/filebrowser.html');
 });
-baseRouter.post('/files', function(req, res, next){
-  var password = req.body.password;
-  var options = {
-    username: 'abc',
-    password: password,
-  };
-  pamAuthenticate(options, function(err, code) {
-    if (!err) {
-      next();	
-    } else {
-      res.send('Unauthorized');
-      res.end();
+// Websocket comms //
+io = socketIO(http, {path: baseUrl + 'files/socket.io',maxHttpBufferSize: 1e8});
+io.on('connection', async function (socket) {
+  let id = socket.id;
+  var authData = {id: false};
+
+  //// Functions ////
+
+  // Check auth
+  async function checkAuth(data) {
+    let password = data[0];
+    let directory = data[1];
+    let options = {
+      username: USER,
+      password: password,
+    };
+    try {
+      await pamAuthenticatePromise(options);
+      authData = {id: true};
+      getFiles(directory);
+    } catch(e) {
+      authData = {id: false};
+      send('badauth');
     }
-  });
+  }
+
+  // Emit to user
+  function send(command, data) {
+    io.sockets.to(id).emit(command, data);
+  }
+
+  // Get file list for directory
+  async function getFiles(directory) {
+    if (! authData.id == true) {
+      return;
+    };
+    let items = await fsw.readdir(directory);
+    if (items.length > 0) {
+      let dirs = [];
+      let files = [];
+      for await (let item of items) {
+        let fullPath = directory + '/' + item;
+        if (fs.lstatSync(fullPath).isDirectory()) {
+          dirs.push(item);
+        } else {
+          files.push(item);
+        }
+      }
+      send('renderfiles', [dirs, files, directory]);
+    } else {
+      send('renderfiles', [[], [], directory]);
+    }
+  }
+
+  // Send file to client
+  async function downloadFile(file) {
+    if (! authData.id == true) {
+      return;
+    };
+    let fileName = file.split('/').slice(-1)[0];
+    let data = await fsw.readFile(file);
+    send('sendfile', [data, fileName]);
+  }
+
+  // Write client sent file
+  async function uploadFile(res) {
+    if (! authData.id == true) {
+      return;
+    };
+    let directory = res[0];
+    let filePath = res[1];
+    let data = res[2];
+    let render = res[3];
+    await fsw.appendFile(filePath, Buffer.from(data));
+    if (render) {
+      getFiles(directory);
+    }
+  }
+
+  // Delete files
+  async function deleteFiles(res) {
+    if (! authData.id == true) {
+      return;
+    };
+    let item = res[0];
+    let directory = res[1];
+    item = item.replace("|","'"); 
+    if (fs.lstatSync(item).isDirectory()) {
+      await fsw.rmdir(item, {recursive: true});
+    } else {
+      await fsw.unlink(item);
+    }
+    getFiles(directory);
+  }
+
+  // Create a folder
+  async function createFolder(res) {
+    if (! authData.id == true) {
+      return;
+    };
+    let dir = res[0];
+    let directory = res[1];
+    if (!fs.existsSync(dir)){
+      await fsw.mkdir(dir);
+    }
+    getFiles(directory);
+  }
+
+  // Incoming socket requests
+  socket.on('auth', checkAuth);
+  socket.on('getfiles', getFiles);
+  socket.on('downloadfile', downloadFile);
+  socket.on('uploadfile', uploadFile);
+  socket.on('deletefiles', deleteFiles);
+  socket.on('createfolder', createFolder);
 });
 
 // Spin up application on CUSTOM_PORT with fallback to port 3000
-app.use(baseurl, baseRouter);
+app.use(baseUrl, baseRouter);
 http.listen(CUSTOM_PORT, function(){
   console.log('listening on *:' + CUSTOM_PORT);
 });
--- public/js/filebrowser.js
+++ public/js/filebrowser.js
@@ -1,1 +1,184 @@
+var homeDir = '/config';
+var host = window.location.hostname; 
+var port = window.location.port;
+var protocol = window.location.protocol;
+var path = window.location.pathname;
+var socket = io(protocol + '//' + host + ':' + port, { path: path + '/socket.io'});
 
+// Auth on connect
+socket.on('connect',function(){
+  $('#filebrowser').empty();
+  $('#filebrowser').append($('<div>').attr('id','loading'));
+  socket.emit('auth', [$('#pass').val(), homeDir]);
+});
+
+// Get file list
+function getFiles(directory) {
+  directory = directory.replace("//","/");
+  directory = directory.replace("|","'");
+  let directoryClean = directory.replace("'","|");
+  if ((directory !== '/') && (directory.endsWith('/'))) {
+    directory = directory.slice(0, -1);
+  }
+  $('#filebrowser').empty();
+  $('#filebrowser').append($('<div>').attr('id','loading'));
+  socket.emit('getfiles', directory);
+}
+
+// Render retry auth
+function badAuth() {
+  $('#filebrowser').empty();
+  let message = $('<p>').text('Unable to authenticate change password and ');
+  let tryButton = $('<button>').attr('onclick', 'reLog()').text('try again');
+  message.append(tryButton);
+  $('#filebrowser').append(message);
+}
+
+// Re-try auth
+function reLog() {
+  $('#filebrowser').empty();
+  $('#filebrowser').append($('<div>').attr('id','loading'));
+  socket.disconnect();
+  socket.connect();
+}
+
+// Render file list
+async function renderFiles(data) {
+  let dirs = data[0];
+  let files = data[1];
+  let directory = data[2];
+  let baseName = directory.split('/').slice(-1)[0]; 
+  let parentFolder = directory.replace(baseName,'');
+  let parentLink = $('<td>').addClass('directory').attr('onclick', 'getFiles(\'' + parentFolder + '\');').text('..');
+  let directoryClean = directory.replace("'","|");
+  if (directoryClean == '/') {
+    directoryClean = '';
+  }
+  let table = $('<table>').addClass('fileTable');
+  let tableHeader = $('<tr>');
+  for await (name of ['Name', 'Type', 'Delete (NO WARNING)']) {
+    tableHeader.append($('<th>').text(name));
+  }
+  let parentRow = $('<tr>');
+  for await (item of [parentLink, $('<td>').text('Parent'), $('<td>')]) {
+    parentRow.append(item);
+  }
+  table.append(tableHeader,parentRow);
+  $('#filebrowser').empty();
+  $('#filebrowser').data('directory', directory);
+  $('#filebrowser').append($('<div>').text(directory));
+  $('#filebrowser').append(table);
+  if (dirs.length > 0) {
+    for await (let dir of dirs) {
+      let tableRow = $('<tr>');
+      let dirClean = dir.replace("'","|");
+      let link = $('<td>').addClass('directory').attr('onclick', 'getFiles(\'' + directoryClean + '/' + dirClean + '\');').text(dir);
+      let type = $('<td>').text('Dir');
+      let del = $('<td>').append($('<button>').addClass('deleteButton').attr('onclick', 'deleter(\'' + directoryClean + '/' + dirClean + '\');').text('Delete'));
+      for await (item of [link, type, del]) {
+        tableRow.append(item);
+      }
+      table.append(tableRow);
+    }
+  }
+  if (files.length > 0) {
+    for await (let file of files) {
+      let tableRow = $('<tr>');
+      let fileClean = file.replace("'","|");
+      let link = $('<td>').addClass('file').attr('onclick', 'downloadFile(\'' + directoryClean + '/' + fileClean + '\');').text(file);
+      let type = $('<td>').text('File');
+      let del = $('<td>').append($('<button>').addClass('deleteButton').attr('onclick', 'deleter(\'' + directoryClean + '/' + fileClean + '\');').text('Delete'));
+      for await (item of [link, type, del]) {
+        tableRow.append(item);
+      }
+      table.append(tableRow);
+    }
+  }
+}
+
+// Download a file
+function downloadFile(file) {
+  file = file.replace("|","'");
+  socket.emit('downloadfile', file);
+}
+
+// Send buffer to download blob
+function sendFile(res) {
+  let data = res[0];
+  let fileName = res[1];
+  let blob = new Blob([data], { type: "application/octetstream" });
+  let url = window.URL || window.webkitURL;
+  link = url.createObjectURL(blob);
+  let a = $("<a />");
+  a.attr("download", fileName);
+  a.attr("href", link);
+  $("body").append(a);
+  a[0].click();
+  $("body").remove(a);
+}
+
+// Upload files to current directory
+async function upload(input) {
+  let directory = $('#filebrowser').data('directory');
+  if (directory == '/') {
+    directoryUp = '';
+  } else {
+    directoryUp = directory;
+  }
+  if (input.files && input.files[0]) {
+    $('#filebrowser').empty();
+    $('#filebrowser').append($('<div>').attr('id','loading'));
+    for await (let file of input.files) {
+      let reader = new FileReader();
+      reader.onload = async function(e) {
+        let fileName = file.name;
+        if (e.total < 100000000) {
+          let data = e.target.result;
+          $('#filebrowser').append($('<div>').text('Uploading ' + fileName));
+          if (file == input.files[input.files.length - 1]) {
+            socket.emit('uploadfile', [directory, directoryUp + '/' + fileName, data, true]);
+          } else {
+            socket.emit('uploadfile', [directory, directoryUp + '/' + fileName, data, false]);
+          }
+        } else {
+          $('#filebrowser').append($('<div>').text('File too big ' + fileName));
+          await new Promise(resolve => setTimeout(resolve, 2000));
+          socket.emit('getfiles', directory);
+        }
+      }
+      reader.readAsArrayBuffer(file);
+    }
+  }
+}
+
+// Delete file/folder
+function deleter(item) {
+  let directory = $('#filebrowser').data('directory');
+  $('#filebrowser').empty();
+  $('#filebrowser').append($('<div>').attr('id','loading'));
+  socket.emit('deletefiles', [item, directory]);
+}
+
+// Delete file/folder
+function createFolder() {
+  let directory = $('#filebrowser').data('directory');
+  if (directory == '/') {
+    directoryUp = '';
+  } else {
+    directoryUp = directory;
+  }
+  let folderName = $('#folderName').val();
+  $('#folderName').val('');
+  if ((folderName.length == 0) || (folderName.includes('/'))) {
+    alert('Bad or Null Directory Name');
+    return '';
+  }
+  $('#filebrowser').empty();
+  $('#filebrowser').append($('<div>').attr('id','loading'));
+  socket.emit('createfolder', [directoryUp + '/' + folderName, directory]);
+}
+
+// Incoming socket requests
+socket.on('renderfiles', renderFiles);
+socket.on('sendfile', sendFile);
+socket.on('badauth', badAuth);
